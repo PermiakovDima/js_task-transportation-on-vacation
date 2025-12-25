dd5c98194069bc5d817647b3ea5f7597be3d24af
Dmytro Permiakov <permiakov.d.s@gmail.com>
2025-12-25 22:16:09 +0200

    fix2

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,17 +4,21 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  const costPerDay = 40;
+  const PRICE_PER_DAY = 40;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISCOUNT = 20;
 
-  if (days >= 7) {
-    return days * costPerDay - 50;
+  if (days >= LONG_TERM) {
+    return days * PRICE_PER_DAY - LONG_TERM_DISCOUNT;
   }
 
-  if (days >= 3 && days < 7) {
-    return days * costPerDay - 20;
+  if (days >= SHORT_TERM && days < LONG_TERM) {
+    return days * PRICE_PER_DAY - SHORT_TERM_DISCOUNT;
   }
 
-  return days * costPerDay;
+  return days * PRICE_PER_DAY;
 }
 
 module.exports = calculateRentalCost;
